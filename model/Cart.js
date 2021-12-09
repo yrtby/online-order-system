a4ec5615762bdc896a0ca02c149a3d7b6a625b59
yrtby <anilcanyurtbay@hotmail.com>
2021-12-10 02:33:24 +0300

    Create Card Model

---
+++ model/Cart.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const CartSchema = new Schema({
+    product_id: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Product",
+        required: true
+    }
+}, { versionKey: false });
+
+const Cart = mongoose.model("Cart",CardSchema);
+module.exports = Cart;
