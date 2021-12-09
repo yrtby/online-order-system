440292954fe22bef7034f78863bb1f7bc2ff6cb0
yrtby <anilcanyurtbay@hotmail.com>
2021-12-10 02:29:35 +0300

    Create Order Model

---
+++ model/Order.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const OrderSchema = new Schema({
+    product_id: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Product",
+        required: true
+    },
+    quantity: {
+        type: Number,
+        required: true
+    }
+}, { versionKey: false });
+
+const Order = mongoose.model("Order",OrderSchema);
+module.exports = Order;
